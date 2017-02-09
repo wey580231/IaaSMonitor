cb2dcfcfb734b1f39983547976ce55253a43b7a5
韩常铭 <han.changming@qq.com>
2017-02-09 10:41:36 +0800

    增加了doLogin()和doGet()方法用于登陆和发起Get请求

---
+++ src/com/RenGu/util/ErrorResponseCodes.java
@@ -0,0 +1,12 @@
+package com.RenGu.util;
+
+        import java.util.HashMap;
+        import java.util.Map;
+
+/**
+ * Created by hanch on 2017/2/9.
+ */
+public class ErrorResponseCodes {
+//    public static Map<String, String> errorResponseCodes = new HashMap<>();
+//    errorResponseCodes
+}
