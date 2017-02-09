d18e42650103909dc2ef524f29311f65127e65ef
yang <wey580231@live.com>
2017-02-09 12:21:10 +0800

    添加json解析

--- src/com/RenGu/servlert/LoginController.java
+++ src/com/RenGu/servlert/LoginController.java
@@ -1,6 +1,9 @@
 package com.RenGu.servlert;
 
 import com.RenGu.util.HttpServers;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-public class MainController extends HttpServlet {
+public class LoginController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -28,11 +31,21 @@ public class MainController extends HttpServlet {
         if (name != null && passWord != null && name.length() > 0
                 && passWord.length() > 0) {
             String token = HttpServers.doLogin(name, passWord,requestUrl);
-            System.out.println(token);
-
             if(token.length() > 0)
             {
-				resp.getWriter().write(token);
+                try {
+                    JSONObject json = new JSONObject(token);
+                    JSONObject obj = json.getJSONObject("access");
+                    if(obj!=null)
+                    {
+                        System.out.println(obj.getJSONObject("token").get("id"));
+                    }
+                } catch (JSONException e) {
+                    System.out.println("解析Json出错！！");
+                    e.printStackTrace();
+                }
+
+                resp.getWriter().write(token);
             }
         }
 
