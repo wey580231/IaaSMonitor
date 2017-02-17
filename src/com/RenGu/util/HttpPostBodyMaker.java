551b9e3dcd914cd1b75b01363d9d116feb8196a4
Fanan <783339208@qq.com>
2017-02-17 09:35:55 +0800

    2017-02-16:增加实例详情

--- src/com/RenGu/util/HttpPostBodyMaker.java
+++ src/com/RenGu/util/HttpPostBodyMaker.java
@@ -1,9 +1,14 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package com.RenGu.util;
 
-/**
- * Created by hanch on 2017/2/9.
- */
 public class HttpPostBodyMaker {
+    public HttpPostBodyMaker() {
+    }
+
     public static String createMultipleServers(String name, String imageRef, String flavorRef, boolean return_reservation_id, int min_count, int max_count) {
         String return_reservation_idNode = "\"return_reservation_id\": \"" + return_reservation_id + "\",";
         String min_countNode = "\"min_count\":\"" + min_count + "\",";
@@ -15,9 +20,10 @@ public class HttpPostBodyMaker {
 
     public static String createServer(String name, String flavorRef, String imageRef, String securityGroupsName, String accessIPv4, String adminPass, String networksUUID) {
         String security_groupsNode = "\"security_groups\":[{\"name\":\"" + securityGroupsName + "\"}],";
-        if (securityGroupsName == null) {
+        if(securityGroupsName == null) {
             security_groupsNode = "";
         }
+
         String networksNode = "\"networks\":[{\"uuid\":\"" + networksUUID + "\"}],";
         String httpPostBody = "{\"server\":{\"" + name + "\":\"new-server-test\",\"flavorRef\":\"" + flavorRef + "\",\"imageRef\":\"" + imageRef + "\"," + security_groupsNode + "\"accessIPv4\":\"" + accessIPv4 + "\",\"adminPass\":\"" + adminPass + "\"," + networksNode + "\"OS-DCF:diskConfig\":\"AUTO\",\"personality\":[{\"path\":\"/etc/banner.txt\",\"contents\":\"ICAgICAgDQoiQSBjbG91ZCBkb2VzIG5vdCBrbm93IHdoeSBp dCBtb3ZlcyBpbiBqdXN0IHN1Y2ggYSBkaXJlY3Rpb24gYW5k IGF0IHN1Y2ggYSBzcGVlZC4uLkl0IGZlZWxzIGFuIGltcHVs c2lvbi4uLnRoaXMgaXMgdGhlIHBsYWNlIHRvIGdvIG5vdy4g QnV0IHRoZSBza3kga25vd3MgdGhlIHJlYXNvbnMgYW5kIHRo ZSBwYXR0ZXJucyBiZWhpbmQgYWxsIGNsb3VkcywgYW5kIHlv dSB3aWxsIGtub3csIHRvbywgd2hlbiB5b3UgbGlmdCB5b3Vy c2VsZiBoaWdoIGVub3VnaCB0byBzZWUgYmV5b25kIGhvcml6 b25zLiINCg0KLVJpY2hhcmQgQmFjaA==\"}]},\"OS-SCH-HNT:scheduler_hints\":{\"same_host\":\"48e6a9f6-30af-47e0-bc04-acaed113bb4e\"}}";
         return httpPostBody;
